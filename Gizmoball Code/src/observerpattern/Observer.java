9710a94ea2c14624379c7e654ea1ee808f736745
Christopher Richmond CESM2015 <christopher.richmond.2015@uni.strath.ac.uk>
2018-01-31 15:42:02 +0000

    added a custom Observer Pattern template packe

---
+++ Gizmoball Code/src/observerpattern/Observer.java
@@ -0,0 +1,10 @@
+package observerpattern;
+
+public interface Observer {
+
+    /**
+     * Called by an Observable object upon this Observer object in order for it to carry out necessary update operations
+     * @param o the Observable object calling this method externally
+     */
+    void update(Observable o);
+}
