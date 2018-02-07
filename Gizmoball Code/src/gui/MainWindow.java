fc0cff14d56e28c359d6b40b1490f481c7a9accb
Christopher Richmond CESM2015 <christopher.richmond.2015@uni.strath.ac.uk>
2018-02-07 15:50:32 +0000

    added initial gui files

---
+++ Gizmoball Code/src/gui/MainWindow.java
@@ -0,0 +1,17 @@
+package gui;
+
+import observerpattern.Observable;
+import observerpattern.Observer;
+
+import javax.swing.*;
+
+public class MainWindow implements Observer{
+
+    private JFrame mainFrame;
+    private JPanel
+
+    @Override
+    public void update(Observable o) {
+
+    }
+}
