9a1f2e3f754eb1e370489127248ef2b18edb9275
Christopher Richmond CESM2015 <christopher.richmond.2015@uni.strath.ac.uk>
2018-02-08 13:27:31 +0000

    Update Menu.java

--- Gizmoball Code/src/gui/Menu.java
+++ Gizmoball Code/src/gui/Menu.java
@@ -23,11 +23,39 @@ public class Menu implements Observer{
         isBuildMode = false;
         buildMenuPanel = new JPanel();
         runMenuPanel = new JPanel();
+        
+        buildMenuPanel.setVisible(false);
+        runMenuPanel.setVisible(true);
     }
+    
+    /**
+     * Only to be accessed internally; this method checks which mode the menu is in and changes the visibilty of the panels accordingly
+     * 
+     * Observer Pattern should ensure this is called every time the system updates 
+     * to check for a potential change from build mode to run mode or vice versa
+     **/
+    private void checkVisibility(){
+        if(isBuildMode){
+            menu.getBuildMenuPanel().setVisible(true);
+            menu.getRunMenuPanel().setVisible(false);
+        }else{
+            menu.getBuildMenuPanel().setVisible(false);
+            menu.getRunMenuPanel().setVisible(true);
+        }
+    }
+    
 
-
+    public void getBuildMenuPanel(){
+        return buildMenuPanel;
+    }
+    
+    public void getRunMenuPanel(){
+        return runMenuPanel;
+    }
+    
     @Override
     public void update() {
         isBuildMode = model.isBuildMode();
+        checkVisibility();
     }
 }
