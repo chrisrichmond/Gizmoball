b2334beaef7d70b929261d5ff984db273cd8377f
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-03-07 17:03:59 +0000

    JUnit testing package

---
+++ Gizmoball Code/src/Testing/TestModel.java
@@ -0,0 +1,23 @@
+package Testing;
+
+import Model.ModelAPI;
+import Model.Model;
+import org.junit.jupiter.api.Test;
+
+ class ModelTest {
+
+    private ModelAPI m1;
+
+
+    public void setUp(){
+        m1 = new Model();
+    }
+
+    @Test
+    public void testLoadFile(){
+
+    }
+
+
+
+}
