ee445d6389b7e9b06f5f6ddf89c58cf689a2a263
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-03-13 13:07:11 +0000

    added first 2 unit tests to test adding gizmos to the model, will continue and add more of these later

--- Gizmoball Code/src/Testing/TestModel.java
+++ Gizmoball Code/src/Testing/TestModel.java
@@ -1,34 +1,46 @@
-//package Testing;
-//
-//import Model.ModelAPI;
-//import Model.Model;
-//import Model.gizmos.*;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.assertNull;
-//
-//class ModelTest {
-//
-//    private ModelAPI m1;
-//    private List<Gizmo> gizmos;
-//
-//    public void setUp(){
-//        m1 = new Model();
-//        this.gizmos = new ArrayList<Gizmo>();
-//    }
-//
-//    @Test
-//    private void testAddingNullGizmoBall(){
-//        System.out.println("Adding ball to JUnit test");
-//        assertNull(m1.getBall());
-//    }
-//
-//    @Test
-//    private void testAddingGizmoBall(){
-//        m1.addGizmo(Gizmo circle);
-//    }
-//
-// }
+package Testing;
+import Model.Model;
+import Model.ModelAPI;
+import Model.gizmos.CircularBumper;
+import Model.gizmos.Gizmo;
+import Model.gizmos.SquareBumper;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ModelTest {
+
+    private ModelAPI m1;
+    private List<Gizmo> gizmos;
+    private Gizmo circle = new CircularBumper("5", 5, 5);
+
+    private Gizmo square = new SquareBumper("1", 1, 1);
+
+    @BeforeAll
+    public  void setUp(){
+        m1 = new Model();
+    }
+
+    @Test
+    public void testGizmoAddCircle(){
+        m1.addGizmo(circle);
+        assertEquals(m1.getGizmos().size(), 1);
+        assertEquals(m1.getCircles().size(), 1);
+    }
+
+    @Test
+    public void testGizmoAddSquare(){
+        m1.addGizmo(square);
+        assertEquals(m1.getGizmos().size(), 1);
+        assertEquals(m1.getSquares().size(), 1);
+
+    }
+
+ }
