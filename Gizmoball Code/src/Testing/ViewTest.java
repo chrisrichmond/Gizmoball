da53a9eff971f428b3d4f356df3ff38e30b6efa2
Steven Barry SE2015 <steven.barry.2016@uni.strath.ac.uk>
2018-03-16 16:34:41 +0000

    began testing the view

--- Gizmoball Code/src/Testing/ViewTest.java
+++ Gizmoball Code/src/Testing/ViewTest.java
@@ -1,7 +1,31 @@
 package Testing;
 
+import Model.Model;
+import Model.ModelAPI;
+import gui.View;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class ViewTest {
 
+    private ModelAPI m1;
+    private View myView;
 
+    @BeforeAll
+    public  void setUp(){
+        m1 = new Model();
+        myView = new View(m1);
+    }
+
+    @Test
+    public void testGetPPL(){
+        assertEquals(25, myView.getPpl());
+    }
 
 }
