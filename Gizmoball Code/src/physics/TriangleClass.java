c6665e078f6c1f69609dd68ee18c0b780ccf1252
Christopher Richmond CESM2015 <christopher.richmond.2015@uni.strath.ac.uk>
2018-03-20 18:00:20 +0000

    fixed triangle rotation to load murray's example gizmo file properly (you'll have to delete your default.gizmo file in your home directory to get it to update)

--- Gizmoball Code/src/physics/TriangleClass.java
+++ Gizmoball Code/src/physics/TriangleClass.java
@@ -25,21 +25,21 @@ public class TriangleClass  {
         this.Y = Y;
         this.rotation = rotation;
 
-        if(rotation == 0) {
+        if(rotation == 3) {
             hypo = new LineSegment(X, Y, (double) (X + size), (double) (Y + size));
             bottom = new LineSegment(X, Y, X, (double) (Y + size));
             side = new LineSegment(X, (double) (Y + size), (double) (X + size), (double) (Y + size));
             angle = new Circle(X, Y, 0);
             top = new Circle(X, (double) (Y + size), 0);
             right = new Circle((double) (X + size), (double) (Y + size), 0);
-        }else if(rotation == 1){
+        }else if(rotation == 0){
             hypo = new LineSegment((double)(X + size), Y, X, (double) (Y + size));
             bottom = new LineSegment(X, Y,(double)(X + size), Y);
             side = new LineSegment(X, Y, X, (double) (Y + size));
             angle = new Circle((double) (X + size), Y, 0);
             top = new Circle(X, Y, 0);
             right = new Circle(X, (double) (Y + size), 0);
-        }else if(rotation == 2) {
+        }else if(rotation == 1) {
             hypo = new LineSegment(X, Y, (double) (X + size), (double) (Y + size));
             bottom = new LineSegment((double) (X + size), Y, (double) (X + size), (double) (Y + size));
             side = new LineSegment(X, Y, (double) (X + size), Y);
@@ -72,11 +72,11 @@ public class TriangleClass  {
 //    }
 
     public Polygon makeTriangle() {
-        if(rotation == 0) {
+        if(rotation == 3) {
             return new Polygon(new int[]{X, (X + size), X}, new int[]{Y, Y + size, Y + size}, 3);
-        }else if(rotation == 1){
+        }else if(rotation == 0){
             return new Polygon(new int[]{X, (X + size), X}, new int[]{Y, Y , Y + size}, 3);
-        }else if(rotation == 2){
+        }else if(rotation == 1){
             return new Polygon(new int[]{X, (X + size), (X + size)}, new int[]{Y, Y , Y + size}, 3);
         }else{
             return new Polygon(new int[]{X, (X + size), (X + size)}, new int[]{Y + size, Y , Y + size}, 3);
